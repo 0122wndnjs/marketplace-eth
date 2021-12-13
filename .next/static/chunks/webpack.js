c8a035a44ebd1f134418cfef6ea59be3adbe1221
Joowon Kim <0122wndnjs@gmail.com>
2021-12-13 12:07:40 +0900

    loading Provider

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13063b67d545b30c"; }
+/******/ 		__webpack_require__.h = function() { return "455a4e908f166709"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ import { getAllCourses } from "@content/courses/fetcher";
 import { useWeb3 } from "@components/providers";
 
 export default function Home({courses}) {
-  const { test } = useWeb3()
+  const { web3, isLoading } = useWeb3()
   return (
     <>
-      { test }
+      { isLoading ? "IS Loading Web3..." : web3 ? "Web3 Ready!" : "Please install Metamask" }
       <Hero />
       <CourseList 
         courses={courses} 
