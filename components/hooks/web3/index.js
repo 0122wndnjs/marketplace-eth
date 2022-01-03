b14dda023d472ea102cb408806b88d03e9273feb
Joowon Kim <0122wndnjs@gmail.com>
2022-01-03 11:22:47 +0900

    Adding ownership of Courses

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -29,6 +29,13 @@ export const useOwnedCourses = (...args) => {
   }
 }
 
+export const useOwnedCourse = (...args) => {
+  const swrRes = enhanceHook(useHooks(hooks => hooks.useOwnedCourse)(...args))
+  return {
+    ownedCourse: swrRes
+  }
+}
+
 export const useWalletInfo = () => {
   const { account } = useAccount()
   const { network } = useNetwork()
