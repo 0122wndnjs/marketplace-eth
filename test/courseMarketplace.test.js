9a055599327cd65dd32c220a9274314b022004cb
Joowon Kim <0122wndnjs@gmail.com>
2022-01-10 15:38:07 +0900

    withdraw test

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -238,7 +238,6 @@ contract("CourseMarketplace", accounts => {
     })
 
     describe("Receive funds", () => {
-
         it("should have transacted funds", async () => {
             const contractBeforeTx = await getBalance(_contract.address)
             const value = "100000000000000000"
@@ -250,7 +249,6 @@ contract("CourseMarketplace", accounts => {
             })
 
             const contractAfterTx = await getBalance(_contract.address)
-
             assert.equal(
                 toBN(contractBeforeTx).add(toBN(value)).toString(),
                 contractAfterTx,
@@ -259,4 +257,40 @@ contract("CourseMarketplace", accounts => {
         })
     })
 
+    describe("Normal withdraw", () => {
+        const fundsToDeposit = "100000000000000000"
+        const overLimitFunds = "999999000000000000000"
+        let currentOwner = null
+
+        before(async () => {
+            currentOwner = await _contract.getContractOwner()
+            await web3.eth.sendTransaction({
+                from: buyer,
+                to: _contract.address,
+                value: fundsToDeposit
+            })
+        })
+        
+        it("should fail when withdrawing with NOT owner address", async () => {
+            const value = "10000000000000000"
+            await catchRevert(_contract.withdraw(value, { from: buyer }))
+        })
+
+        it("should fail when withdrawing OVER limit balance", async () => {
+            await catchRevert(_contract.withdraw(overLimitFunds, { from: contractOwner }))
+        })
+
+        it("should have +0.1ETH after withdraw", async () => {
+            const ownerBalance = await getBalance(currentOwner)
+            const result = await _contract.withdraw(fundsToDeposit, { from: currentOwner })
+            const newOwnerBalance = await getBalance(currentOwner)
+            const gas = await getGas(result)
+
+            assert.equal(
+                toBN(ownerBalance).add(toBN(fundsToDeposit)).sub(toBN(gas)).toString(),
+                newOwnerBalance,
+                "The new owner balance is not correct!"
+            )
+        })
+    })
 })
